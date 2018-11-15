e0899c7b857acc1277e73312fea0c7803c013400
ka <kaseidooun@gmail.com>
2018-11-15 16:04:33 +0900

    课内练习 20181115

--- jQuery/API/isUserExist.php
+++ jQuery/API/isUserExist.php
@@ -4,11 +4,28 @@ $users = [
     'lisi'
 ];
 
+$errs = [
+    'userName'=>'',
+    'age'=>''
+];
+
 $userName = $_GET["userName"];
 
+
+
+
 if (in_array($userName, $users)){
-    echo "true";
-} else {
-    echo "false";
+    $errs['userName'] = "用户已存在";
+} else if(empty($userName)){
+    $errs['userName'] = "用户名不能为空";
+} else if (strpos($userName, "请输入手机号")!==false){
+    $errs['userName'] = "不能包含敏感文字";
 }
+
+if ($_GET["age"]<18) {
+    $errs['age'] = "用户必须年满18周岁";
+}
+
+
+echo json_encode($errs);
 ?>
