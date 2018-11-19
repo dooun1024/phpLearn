36043197a8c15fdedea53c078a81765abb84e8a8
ka <kaseidooun@gmail.com>
2018-11-19 16:27:50 +0900

    导入bootstrap , jquery 等

---
+++ php_DBConnect/element/header.php
@@ -0,0 +1,9 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!-- 导入jQuery 文件 -->
+<script src="js/jquery-3.3.1.js"></script>
+<!-- 导入bootstrap 文件 -->
+<link rel="stylesheet" href="css/bootstrap.css">
+<script src="js/bootstrap.bundle.js" charset="utf-8"></script>
+<script src="js/bootstrap.js" charset="utf-8"></script>
